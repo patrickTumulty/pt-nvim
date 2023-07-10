1e204e4789fa1ba62779b7e51714a1f76dcee83f
ptumulty <ptumulty1@gmail.com>
2023-07-09 17:48:24 -0700

    Add pane navigation

--- lua/pt-nvim/remap.lua
+++ lua/pt-nvim/remap.lua
@@ -5,6 +5,11 @@ vim.keymap.set("i", "<S-TAB>", "<C-d>")
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Go to netrw directory listing" })
 
+vim.keymap.set("n", "<C-h>", "<C-w><left>", { desc = "Move to split pane left" })
+vim.keymap.set("n", "<C-j>", "<C-w><down>", { desc = "Move to split pane down" })
+vim.keymap.set("n", "<C-k>", "<C-w><up>", { desc = "Move to split pane up" })
+vim.keymap.set("n", "<C-l>", "<C-w><right>", { desc = "Move to split pane right" })
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
