98daa174b8697e400699c43db9ae5b75d7635ca4
Patrick <ptumulty1@gmail.com>
2024-11-07 23:24:02 -0800

    Refine xdotool logic to only refresh the target HTML page

--- lua/pt-nvim/xdotool.lua
+++ lua/pt-nvim/xdotool.lua
@@ -8,18 +8,41 @@ local function is_xdotool_installed()
     return result ~= ""
 end
 
-local browser = "chrome" -- or 'firefox'
-
 -- Get the current active window and store that in tmp file
 local nvim_window_id_cmd = "xdotool getactivewindow > /tmp/nvim_window_id"
--- Refresh browser
-local browser_refresh_cmd = string.format("xdotool search --onlyvisible --class '%s' windowactivate --sync key F5", browser)
 -- Use stored window id to refocus the window
 local refocus_nvim_cmd = "xdotool windowactivate $(cat /tmp/nvim_window_id)"
 
-if is_xdotool_installed() then
-    vim.api.nvim_create_autocmd("BufWritePost", {
-        pattern = "*.html",
-        command = string.format("silent! !%s && %s && %s", nvim_window_id_cmd, browser_refresh_cmd, refocus_nvim_cmd)
-    })
+local function refresh_page(title)
+    local refresh_cmd = string.format("xdotool search --onlyvisible --name '%s' windowactivate --sync key F5", title)
+
+    local command = string.format("silent! !%s && %s && %s", nvim_window_id_cmd, refresh_cmd,
+        refocus_nvim_cmd)
+
+    vim.api.nvim_command(command)
 end
+
+
+if is_xdotool_installed() then
+    return
+end
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*.html",
+    callback = function()
+        -- [[
+        -- We are getting the first 20 lines of HTML and parsing
+        -- out the title. Once we have that information, we can
+        -- use xdotool to refresh that webpage if it is open.
+        -- ]]
+        local content = vim.fn.getline(1, math.min(20, vim.fn.line("$")))
+        local full_conent = table.concat(content, "\n")
+        local title = full_conent:match("<title>(.-)</title>")
+
+        if not title then
+            return
+        end
+
+        refresh_page(title)
+    end
+})
