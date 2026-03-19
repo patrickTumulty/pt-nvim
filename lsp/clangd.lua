600924837b985b506e5a5c3822f5f7e15c1d8775
ptumulty <ptumulty@quantumdrones.com>
2026-03-19 15:08:12 -0700

    clangd config

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -2,7 +2,10 @@
 -- Then moved directory to .local/lsp and renamed to clangd
 --
 return {
-    cmd = { utils.mason_bin_path("clangd"), '--background-index' },
+    cmd = { utils.mason_bin_path("clangd"),
+        '--background-index',
+        "--query-driver=/usr/bin/g++*,/usr/bin/gcc*,/opt/nos/sysroots/x86_64-tdsdk-linux/usr/bin/aarch64-oe4t-linux/aarch64-oe4t-linux-g*,"
+    },
     root_markers = { 'compile_commands.json', 'compile_flags.txt' },
     filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
     single_file_support = true,
