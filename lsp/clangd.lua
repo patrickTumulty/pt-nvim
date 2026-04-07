f6b23d74da4452bcfd23dd6791c673aa745d3b24
Patrick <ptumulty1@gmail.com>
2026-04-06 21:50:26 -0700

    update clangd config

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -1,10 +1,22 @@
 -- sudo wget https://github.com/clangd/clangd/releases/download/19.1.2/clangd-linux-19.1.2.zip
 -- Then moved directory to .local/lsp and renamed to clangd
---
+
+
+local home = os.getenv("HOME")
+
+local drivers = {
+    home .. "/.espressif/tools/**/bin/*gcc",
+    "/usr/bin/*gcc",
+    "/usr/bin/*g++",
+    "/opt/nos/sysroots/**/bin/*gcc",
+    "/opt/nos/sysroots/**/bin/*g++",
+}
+
 return {
-    cmd = { utils.mason_bin_path("clangd"),
-        '--background-index',
-        "--query-driver=/usr/bin/g++*,/usr/bin/gcc*,/opt/nos/sysroots/x86_64-tdsdk-linux/usr/bin/aarch64-oe4t-linux/aarch64-oe4t-linux-g*"
+    cmd = {
+        utils.mason_bin_path("clangd"),
+        "--background-index",
+        "--query-driver=" .. table.concat(drivers, ",")
     },
     root_markers = { 'compile_commands.json', 'compile_flags.txt' },
     filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
