8cae2a77d1162490871ce074b4457e32ee07a5a9
ptumulty <ptumulty@quantumdrones.com>
2026-09-19 01:25:44 +0200

    Update plugins

--- lua/config/multigrep.lua
+++ lua/config/multigrep.lua
@@ -29,8 +29,16 @@ local live_multigrep = function(opts)
 
             ---@diagnostic disable-next-line: deprecated
             return vim.tbl_flatten {
-                args,
-                { "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
+                args, {
+                "--hidden",
+                "--glob", "!.git/*",
+                "--color=never",
+                "--no-heading",
+                "--with-filename",
+                "--line-number",
+                "--column",
+                "--smart-case"
+            },
             }
         end,
         entry_maker = make_entry.gen_from_vimgrep(opts),
